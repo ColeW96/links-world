6702bd090c760154a1d81c746e8c66ce5be6a64a
ColeW96 <135039107+ColeW96@users.noreply.github.com>
2025-11-03 17:08:04 -0500

    Add Drag and Drop
    adds drag and drop to inventory with mouse and gamepad item swapping

--- GUI/pause_menu/Inventory/Scripts/inventory_ui.gd
+++ GUI/pause_menu/Inventory/Scripts/inventory_ui.gd
@@ -3,6 +3,11 @@ class_name InventoryUI extends Control
 const INVENTORY_SLOT = preload("res://GUI/pause_menu/Inventory/inventory_slot.tscn")
 
 var focus_index : int = 0
+var hovered_item : InventorySlotUI
+var swap_item : InventorySlotUI
+var focus_item : InventorySlotUI
+
+var swapping : bool = false
 
 @export var data : InventoryData
 
@@ -18,8 +23,30 @@ func _ready() -> void:
 	data.changed.connect( on_inventory_changed )
 	data.equipment_changed.connect( on_inventory_changed )
 	pass
-	
-	
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ability"):
+		if swapping == false:
+			swap_item = focus_item
+			
+			if swap_item.slot_data == null:
+				return
+			
+			swap_item.texture_rect.modulate.a = 0.5
+			swapping = true
+		elif swapping == true:
+			data.swap_items_by_index( swap_item.get_index(), focus_item.get_index() )
+			update_inventory( false )
+			PauseMenu.update_item_description( focus_item.slot_data.item_data.description )
+			swap_item.texture_rect.modulate.a = 1
+			swapping = false
+		
+	if event.is_action_pressed("attack"):
+			swap_item.texture_rect.modulate.a = 1
+			swapping = false
+
+
 func clear_inventory() -> void:
 	for c in get_children():
 		c.set_slot_data( null )
@@ -34,6 +61,7 @@ func update_inventory( apply_focus : bool = true ) -> void:
 	for i in inventory_slots.size():
 		var slot : InventorySlotUI = get_child( i )
 		slot.set_slot_data( inventory_slots[ i ] )
+		connect_item_signals( slot )
 	
 	# update equipment slots
 	var e_slots : Array[ SlotData ] = data.equipment_slots()
@@ -56,4 +84,43 @@ func item_focused() -> void:
 
 func on_inventory_changed() -> void:
 	update_inventory( false )
+	pass
+
+
+func connect_item_signals( item : InventorySlotUI ) -> void:
+	if not item.button_up.is_connected( _on_item_drop ):
+		item.button_up.connect( _on_item_drop.bind( item ) )
+	
+	if not item.mouse_entered.is_connected( _on_item_mouse_entered ):
+		item.mouse_entered.connect( _on_item_mouse_entered.bind( item ) )
+	
+	if not item.mouse_exited.is_connected( _on_item_mouse_exited ):
+		item.mouse_exited.connect( _on_item_mouse_exited )
+		
+	if not item.focus_entered.is_connected( _on_item_focused ):
+		item.focus_entered.connect( _on_item_focused.bind(item) )
+	pass
+
+
+func _on_item_drop( item : InventorySlotUI ) -> void:
+	if item == null or item == hovered_item or hovered_item == null:
+		return
+	data.swap_items_by_index( item.get_index(), hovered_item.get_index() )
+	update_inventory( false )
+	hovered_item.grab_focus()
+	pass
+
+
+func _on_item_mouse_entered( item : InventorySlotUI ) -> void:
+	hovered_item = item
+	pass
+
+
+func _on_item_mouse_exited() -> void:
+	hovered_item = null
+	pass
+
+
+func _on_item_focused( item : InventorySlotUI ) -> void:
+	focus_item = item
 	pass
